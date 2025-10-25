18bebd0d4c145b680e7a499c0285025a60832592
jessicaazariegos2005-rgb <tu_correo_de_github@gmail.com>
2025-10-25 15:33:06 -0600

    taller cambios

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -3,7 +3,21 @@ import { describe, it, expect } from "vitest";
 import { sum } from "../src/utils/sum.js";
 
 describe("sum", () => {
-  it("adds two numbers", () => {
+  it("adds two numbers", () => 
+    {
     expect(sum(2, 3)).toBe(5);
   });
+ 
+ it("adds tree numbers", () => 
+    {
+    expect(sum(3, 3)).toBe(6);
+  });
+
+
+
+
+
+
+
+
 });
