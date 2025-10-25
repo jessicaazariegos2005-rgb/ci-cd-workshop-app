c4ba953cd9ff51de1bd1cb2743aba73c57813b8d
jessicaazariegos2005-rgb <tu_correo_de_github@gmail.com>
2025-10-24 23:21:01 -0600

    taller prueba

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
-// src/server.ts
 import app from "./index.js";
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`[server] listening on http://localhost:${PORT}`);
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
